09575f9dca7e92721588e3bb91dc1f776b61556c
Dave Burke <dburke84@gmail.com>
2020-12-01 20:55:09 -0600

    Add day2 template with new file reading utility

---
+++ day2.js
@@ -0,0 +1,9 @@
+const { readFile } = require('fs')
+const utils = require('./utils')
+
+async function part1() {
+    const data = await utils.readInput('day2.txt');
+    console.log(data);
+}
+
+part1();
---
+++ utils.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+module.exports = {
+
+    readInputRaw: async function(file) {
+        const promise = new Promise((resolve, reject) => {
+            fs.readFile(file, 'utf8', function(err, data) {
+                if(err) {
+                    reject(err);
+                }
+                resolve(data);
+            });
+        });
+        return promise;
+    },
+
+    readInput: async function(file) {
+        const data = await this.readInputRaw(file);
+        return data.split('\n');
+    }
+
+}
