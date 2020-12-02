8572e08b6f22825fc7d14c77d5bd0688aa283193
Dave Burke <dburke84@gmail.com>
2020-12-02 08:34:28 -0600

    Update main to run day 3

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const utils = require('./utils')
-const day2 = require('./day2')
+// const day2 = require('./day2')
+const day3 = require('./day3')
 
+// Day 2
+// utils.readInput('day2.txt').then((input) => day2.part2(input)).then(result => console.log(`Done: ${result}`))
 // utils.readInput('day2.txt').then((input) => day2.part1(input)).then(result => console.log(`Done: ${result}`))
-utils.readInput('day2.txt').then((input) => day2.part2(input)).then(result => console.log(`Done: ${result}`))
+
+// Day 3
+utils.readInput('day3.txt').then((input) => day3.part1(input)).then(result => console.log(`Done: ${result}`))
