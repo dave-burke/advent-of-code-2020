9d71800a0e837f1cc8b33e82eb639c62c64c41a8
Dave Burke <dburke84@gmail.com>
2020-12-01 22:04:17 -0600

    Lint utils.js

--- utils.js
+++ utils.js
@@ -1,22 +1,22 @@
-const fs = require('fs');
+const fs = require('fs')
 
 module.exports = {
 
-    readInputRaw: async function(file) {
-        const promise = new Promise((resolve, reject) => {
-            fs.readFile(file, 'utf8', function(err, data) {
-                if(err) {
-                    reject(err);
-                }
-                resolve(data);
-            });
-        });
-        return promise;
-    },
+  readInputRaw: async function (file) {
+    const promise = new Promise((resolve, reject) => {
+      fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+          reject(err)
+        }
+        resolve(data)
+      })
+    })
+    return promise
+  },
 
-    readInput: async function(file) {
-        const data = await this.readInputRaw(file);
-        return data.split('\n');
-    }
+  readInput: async function (file) {
+    const data = await this.readInputRaw(file)
+    return data.split('\n')
+  },
 
 }
