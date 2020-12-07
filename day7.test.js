6d1dc1aad1a73a93e617574d99eab7d9244083dc
Dave Burke <dburke84@gmail.com>
2020-12-07 05:17:39 -0600

    Add day7 input and test input

--- day7.test.js
+++ day7.test.js
@@ -3,9 +3,17 @@ const day7 = require('./day7')
 
 test('part1', () => {
   const testInput = [
-    'a',
+    'light red bags contain 1 bright white bag, 2 muted yellow bags.',
+    'dark orange bags contain 3 bright white bags, 4 muted yellow bags.',
+    'bright white bags contain 1 shiny gold bag.',
+    'muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.',
+    'shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.',
+    'dark olive bags contain 3 faded blue bags, 4 dotted black bags.',
+    'vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.',
+    'faded blue bags contain no other bags.',
+    'dotted black bags contain no other bags.',
   ]
-  expect(day7.part1(testInput)).toBe(testInput)
+  expect(day7.part1(testInput)).toBe(4)
 })
 
 test('part2', () => {
--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const day = 7
 const part = 1
 
 const utils = require('./utils')
-const solution = require(`./day${6}`)
+const solution = require(`./day${day}`)
 
 utils.readInputRaw(`day${day}.txt`).then((input) => solution[`part${part}`](input)).then(result => console.log(`Done: ${result}`))
