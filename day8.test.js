2cb0e4b67df30e00d22d65d6be5195c9f935cd65
Dave Burke <dburke84@gmail.com>
2020-12-08 05:18:12 -0600

    Add day8 part1 test input

--- day8.test.js
+++ day8.test.js
@@ -3,9 +3,17 @@ const day8 = require('./day8')
 
 test('part1', () => {
   const testInput = [
-    'a',
+    'nop +0',
+    'acc +1',
+    'jmp +4',
+    'acc +3',
+    'jmp -3',
+    'acc -99',
+    'acc +1',
+    'jmp -4',
+    'acc +6',
   ]
-  expect(day8.part1(testInput)).toBe(testInput)
+  expect(day8.part1(testInput)).toBe(5)
 })
 
 test('part2', () => {
