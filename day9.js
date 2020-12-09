c89631c1fb4194439ae87eda794b99f8936d952e
Dave Burke <dburke84@gmail.com>
2020-12-09 05:56:55 -0600

    Implement day9 part1

--- day9.js
+++ day9.js
@@ -1,9 +1,44 @@
+function computeSums (input) {
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      const a = Number(input[i])
+      const b = Number(input[j])
+      result.push(a + b)
+    }
+  }
+  return result
+}
+function computeRecentSums (input, start, end) {
+  const preamble = input.slice(start, end)
+  return computeSums(preamble)
+}
+function validate (input, start, end, check) {
+  const sums = computeRecentSums(input, start, end)
+  if (sums.includes(check)) {
+    return true
+  }
+  return false
+}
+
+function checkInputPart1 (input, preambleLength) {
+  for (let i = preambleLength; i <= input.length; i++) {
+    const end = i
+    const start = i - preambleLength
+    const check = Number(input[i])
+    console.log(`calcSums ${start} to ${end} looking for ${check}`)
+    if (!validate(input, start, end, check)) {
+      return check
+    }
+  }
+  return null
+}
 function part1 (input) {
-  return input
+  return checkInputPart1(input, 25)
 }
 
 function part2 (input) {
   return input
 }
 
-module.exports = { part1, part2 }
+module.exports = { part1, part2, computeSums, computeRecentSums, checkInputPart1 }
--- day9.test.js
+++ day9.test.js
@@ -1,11 +1,42 @@
 const { expect, test } = require('@jest/globals')
 const day9 = require('./day9')
 
+test('computeRecentSums', () => {
+  const testInput = [
+    '1',
+    '2',
+    '3',
+  ]
+  const result = day9.computeSums(testInput)
+  expect(result).toHaveLength(3)
+  expect(result).toContain(3)
+  expect(result).toContain(4)
+  expect(result).toContain(5)
+})
 test('part1', () => {
   const testInput = [
-    'a',
+    '35',
+    '20',
+    '15',
+    '25',
+    '47',
+    '40',
+    '62',
+    '55',
+    '65',
+    '95',
+    '102',
+    '117',
+    '150',
+    '182',
+    '127',
+    '219',
+    '299',
+    '277',
+    '309',
+    '576',
   ]
-  expect(day9.part1(testInput)).toBe(testInput)
+  expect(day9.checkInputPart1(testInput, 5)).toBe(127)
 })
 
 test('part2', () => {
