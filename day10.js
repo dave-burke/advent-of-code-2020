7eb2bc1425a755700eea3d787f72e1c01f3d7ec0
Dave Burke <dburke84@gmail.com>
2020-12-10 05:34:36 -0600

    WIP day10

--- day10.js
+++ day10.js
@@ -1,5 +1,28 @@
 function part1 (input) {
-  return input
+  const sorted = input.sort((a, b) => a - b)
+  sorted.unshift(0) // outlet
+  sorted.push(sorted[sorted.length - 1] + 3) // device adapter
+  const processed = sorted.reduce((result, cur, i, arr) => {
+    const next = {
+      prev: cur,
+      nOnes: result.nOnes,
+      nThrees: result.nThrees,
+    }
+    if (result.prev !== undefined) {
+      if (result.prev - next.prev === 1) {
+        next.nOnes++
+      }
+      if (result.prev - next.prev === 3) {
+        next.nThrees++
+      }
+    }
+    return next
+  }, {
+    prev: undefined,
+    nOnes: 0,
+    nThrees: 0,
+  })
+  return processed.nOnes * processed.nThrees
 }
 
 function part2 (input) {
