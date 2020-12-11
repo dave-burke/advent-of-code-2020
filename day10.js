016c7b31577830737bfa1015c039428f266846ae
Dave Burke <dburke84@gmail.com>
2020-12-10 19:54:39 -0600

    WIP part2

--- day10.js
+++ day10.js
@@ -12,7 +12,28 @@ function part1 (input) {
 }
 
 function part2 (input) {
-  return input
+  const sorted = input.map(Number).sort((a, b) => a - b)
+  sorted.unshift(0) // outlet
+  sorted.push(sorted[sorted.length - 1] + 3) // device adapter
+  const groups = []
+  let subgroup = []
+  for (let i = 0; i < sorted.length; i++) {
+    subgroup.push(sorted[i])
+    if (sorted[i + 1] - sorted[i] >= 3) {
+      groups.push(subgroup)
+      subgroup = []
+    }
+  }
+  groups.push(subgroup)
+
+  const groupArrangements = groups.map(subgroup => {
+    if (subgroup.length < 3) {
+      return 1
+    } else {
+      return Math.pow(2, subgroup.length - 2)
+    }
+  }).reduce((a, b) => a * b, 1)
+  return groupArrangements
 }
 
 module.exports = { part1, part2 }
