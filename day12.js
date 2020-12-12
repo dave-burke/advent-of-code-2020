4ef19ef39a93a505a5eccd3c0e9189636293b6ee
Dave Burke <dburke84@gmail.com>
2020-12-12 07:16:23 -0600

    Part1 WIP

--- day12.js
+++ day12.js
@@ -9,8 +9,53 @@
 
 The ship starts by facing east. Only the L and R actions change the direction the ship is facing. (That is, if the ship is facing east and the next instruction is N10, the ship would move north 10 units, but would still move east if the following action were F.)
 */
+class Ship {
+  constructor () {
+    this.directions = ['E', 'S', 'W', 'N']
+    this.x = 0
+    this.y = 0
+    this.degrees = 0
+  }
+
+  get direction () {
+    return this.direction(Math.floor(this.degrees / 90))
+  }
+
+  get manhattanDistance () {
+    return Math.abs(this.x) * Math.abs(this.y)
+  }
+
+  navigate (instruction) {
+    const [op, amount] = instruction.match(/([NSEWFLR])(\d+)/).groups
+    if (this.directions.includes(op)) {
+      this.move(op, amount)
+    }
+    if (op === 'L') this.turn(-1 * amount)
+    if (op === 'R') this.turn(amount)
+    if (op === 'F') this.move(this.direction, amount)
+  }
+
+  move (heading, amount) {
+    if (heading === 'N') this.y++
+    if (heading === 'S') this.y--
+    if (heading === 'E') this.x++
+    if (heading === 'W') this.x--
+  }
+
+  drive (amount) {
+    this.move(this.direction, amount)
+  }
+
+  turn (degrees, amount) {
+    this.degrees += amount
+  }
+}
 function part1 (input) {
-  return input
+  const ship = new Ship()
+  for (const instruction of input) {
+    ship.navigate(instruction)
+  }
+  return ship.manhattanDistance
 }
 
 function part2 (input) {
