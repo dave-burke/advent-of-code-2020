e4dfb7e51ee509a4956a5983cc0fbb68d8b36cd2
Dave Burke <dburke84@gmail.com>
2020-12-11 18:00:20 -0600

    Part1 WIP
    Works for tests, but not input

--- day11.js
+++ day11.js
@@ -1,9 +1,74 @@
+function parse (input) {
+  return input.map(line => line.split(''))
+}
+
+function getNeighbors (seats, y, x) {
+  const neighborCoords = [
+    [y - 1, x - 1],
+    [y - 1, x],
+    [y - 1, x + 1],
+    [y, x - 1],
+    [y, x + 1],
+    [y + 1, x - 1],
+    [y + 1, x],
+    [y + 1, x + 1],
+  ]
+  return neighborCoords
+    .filter(neighbor =>
+      neighbor[0] >= 0 && neighbor[0] < seats[y].length &&
+      neighbor[1] >= 0 && neighbor[1] < seats.length)
+    .map(coords => seats[coords[0]][coords[1]])
+}
+
+function countNeightbors (seats, y, x) {
+  return getNeighbors(seats, y, x)
+    .filter(space => space === '#')
+    .length
+}
+
+function step (seats) {
+  const newSeats = []
+  for (let y = 0; y < seats.length; y++) {
+    const row = seats[y]
+    const newRow = []
+    for (let x = 0; x < row.length; x++) {
+      const seat = seats[y][x]
+      const nNeighbors = countNeightbors(seats, y, x)
+      switch (seat) {
+        case ('L'): // empty seat
+          newRow.push(nNeighbors === 0 ? '#' : 'L')
+          break
+        case ('#'): // filled seat
+          newRow.push(nNeighbors >= 4 ? 'L' : '#')
+          break
+        default:
+          newRow.push(seat)
+      }
+    }
+    newSeats.push(newRow)
+  }
+  return newSeats
+}
+
+function areEqual (seatsA, seatsB) {
+  const flatA = seatsA.flat()
+  const flatB = seatsB.flat()
+  return flatA.length === flatB.length &&
+  flatA.every((seat, i) => seat === flatB[i])
+}
+
 function part1 (input) {
-  return input
+  let prev = []
+  let cur = parse(input)
+  while (!areEqual(prev, cur)) {
+    prev = cur
+    cur = step(cur)
+  }
+  return cur.flat().filter(it => it === '#').length
 }
 
 function part2 (input) {
   return input
 }
 
-module.exports = { part1, part2 }
+module.exports = { part1, part2, parse, countNeightbors, step, getNeighbors, areEqual }
--- day11.test.js
+++ day11.test.js
@@ -1,11 +1,90 @@
 const { expect, test } = require('@jest/globals')
 const day11 = require('./day11')
 
+describe('getNeighbors', () => {
+  test('middle', () => {
+    expect(day11.getNeighbors([
+      ['#', 'L', '#'],
+      ['.', 'L', 'L'],
+      ['#', '.', '#']],
+    1, 1)).toStrictEqual(
+      ['#', 'L', '#', '.', 'L', '#', '.', '#'])
+  })
+  test('corner', () => {
+    expect(day11.getNeighbors([
+      ['#', 'L', '#'],
+      ['.', 'L', 'L'],
+      ['#', '.', '#']],
+    0, 0)).toStrictEqual(
+      ['L', '.', 'L'])
+  })
+})
+
+describe('countNeighbors', () => {
+  test('all neighbors', () => {
+    expect(day11.countNeightbors([
+      ['#', '#', '#'],
+      ['#', 'L', '#'],
+      ['#', '#', '#']],
+    1, 1)).toBe(8)
+  })
+})
+const step0 = [
+  'L.LL.LL.LL',
+  'LLLLLLL.LL',
+  'L.L.L..L..',
+  'LLLL.LL.LL',
+  'L.LL.LL.LL',
+  'L.LLLLL.LL',
+  '..L.L.....',
+  'LLLLLLLLLL',
+  'L.LLLLLL.L',
+  'L.LLLLL.LL',
+]
+const step1 = [
+  '#.##.##.##',
+  '#######.##',
+  '#.#.#..#..',
+  '####.##.##',
+  '#.##.##.##',
+  '#.#####.##',
+  '..#.#.....',
+  '##########',
+  '#.######.#',
+  '#.#####.##',
+]
+
+const step2 = [
+  '#.LL.L#.##',
+  '#LLLLLL.L#',
+  'L.L.L..L..',
+  '#LLL.LL.L#',
+  '#.LL.LL.LL',
+  '#.LLLL#.##',
+  '..L.L.....',
+  '#LLLLLLLL#',
+  '#.LLLLLL.L',
+  '#.#LLLL.##',
+]
+
+test('parse', () => {
+  expect(day11.parse(['#.L'])).toStrictEqual([['#', '.', 'L']])
+})
+
+test('areEqual', () => {
+  expect(day11.areEqual(step0, step0)).toBe(true)
+  expect(day11.areEqual(step1, step1)).toBe(true)
+  expect(day11.areEqual(step2, step2)).toBe(true)
+})
+
+test('step', () => {
+  expect(day11.step(day11.parse(step0)))
+    .toStrictEqual(day11.parse(step1))
+  expect(day11.step(day11.parse(step1)))
+    .toStrictEqual(day11.parse(step2))
+})
 test('part1', () => {
-  const testInput = [
-    'a',
-  ]
-  expect(day11.part1(testInput)).toBe(testInput)
+  expect(day11.part1(step0)).toBe(37)
 })
 
 test('part2', () => {
