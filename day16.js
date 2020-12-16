3abb3bcdbe51d8d14c30e6febea7c36bfed0ec92
Dave Burke <dburke84@gmail.com>
2020-12-16 16:13:48 -0600

    WIP part2

--- day16.js
+++ day16.js
@@ -1,7 +1,7 @@
 function findInvalidNumbers (tickets, validRanges) {
-  return tickets.map(Number).map(numbers => {
-    return numbers.filter(number => {
-      return validRanges.some(range => {
+  return tickets.map(numbers => {
+    return numbers.map(Number).filter(number => {
+      return !validRanges.some(range => {
         const [min, max] = range.split('-')
         return number >= Number(min) && number <= Number(max)
       })
@@ -21,14 +21,36 @@ function part1 (input) {
     .map(spec => spec.ranges)
     .flat()
   const nearbyTickets = nearbyTicketsSection
-    .slice(1)
     .split('\n')
+    .slice(1)
     .map(line => line.split(','))
   return findInvalidNumbers(nearbyTickets, validRanges)
     .reduce((a, b) => a + b, 0)
 }
 
 function part2 (input) {
+  const [fieldsSection, yourTicketSection, nearbyTicketsSection] = input.split('\n\n')
+  const fieldRanges = fieldsSection
+    .split('\n')
+    .reduce((map, next) => {
+      const [fieldName, numberSpec] = next.split(': ')
+      const ranges = numberSpec.split(' or ')
+        .map(spec => {
+          const [min, max] = spec.split('-')
+          return { min, max }
+        })
+      map.set(fieldName, ranges)
+      return map
+    }, new Map())
+  const yourTicket = yourTicketSection
+    .split('\n')[1]
+    .split(',')
+  const nearbyTickets = nearbyTicketsSection
+    .split('\n')
+    .slice(1)
+    .map(ticketLine => ticketLine.split(','))
+
+  const nFields = yourTicket.length
   return input
 }
 
--- day16.test.js
+++ day16.test.js
@@ -1,9 +1,7 @@
 const { expect, test } = require('@jest/globals')
 const day16 = require('./day16')
 
-test('part1', () => {
-  const testInput = `
-class: 1-3 or 5-7
+const testInput = `class: 1-3 or 5-7
 row: 6-11 or 33-44
 seat: 13-40 or 45-50
 
@@ -14,14 +12,12 @@ nearby tickets:
 7,3,47
 40,4,50
 55,2,20
-38,6,12
-`
+38,6,12`
+
+test('part1', () => {
   expect(day16.part1(testInput)).toBe(71)
 })
 
 test('part2', () => {
-  const testInput = [
-    'b',
-  ]
-  expect(day16.part2(testInput)).toBe(testInput)
+  expect(day16.part2(testInput)).toBe(1)
 })
