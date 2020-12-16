0745bd0ad607b3717cba31fbe30c4a41e28238e1
Dave Burke <dburke84@gmail.com>
2020-12-16 05:39:02 -0600

    WIP part1

--- day16.js
+++ day16.js
@@ -1,5 +1,31 @@
+function findInvalidNumbers (tickets, validRanges) {
+  return tickets.map(Number).map(numbers => {
+    return numbers.filter(number => {
+      return validRanges.some(range => {
+        const [min, max] = range.split('-')
+        return number >= Number(min) && number <= Number(max)
+      })
+    })
+  }).flat()
+}
 function part1 (input) {
-  return input
+  // eslint-disable-next-line no-unused-vars
+  const [fieldsSection, _, nearbyTicketsSection] = input.split('\n\n')
+  const validRanges = fieldsSection
+    .split('\n')
+    .map(fieldSpec => {
+      const [fieldName, numberSpec] = fieldSpec.split(': ')
+      const ranges = numberSpec.split(' or ')
+      return { fieldName, ranges }
+    })
+    .map(spec => spec.ranges)
+    .flat()
+  const nearbyTickets = nearbyTicketsSection
+    .slice(1)
+    .split('\n')
+    .map(line => line.split(','))
+  return findInvalidNumbers(nearbyTickets, validRanges)
+    .reduce((a, b) => a + b, 0)
 }
 
 function part2 (input) {
--- day16.test.js
+++ day16.test.js
@@ -2,10 +2,21 @@ const { expect, test } = require('@jest/globals')
 const day16 = require('./day16')
 
 test('part1', () => {
-  const testInput = [
-    'a',
-  ]
-  expect(day16.part1(testInput)).toBe(testInput)
+  const testInput = `
+class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12
+`
+  expect(day16.part1(testInput)).toBe(71)
 })
 
 test('part2', () => {
