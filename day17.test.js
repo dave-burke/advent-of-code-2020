31b93e656149e61982c06c32685230f27d77a76f
Dave Burke <dburke84@gmail.com>
2020-12-17 09:18:54 -0600

    Add part2 test case

--- day17.test.js
+++ day17.test.js
@@ -1,18 +1,16 @@
 const { expect, test } = require('@jest/globals')
 const day17 = require('./day17')
 
+const testInput = [
+  '.#.',
+  '..#',
+  '###',
+]
+
 test('part1', () => {
-  const testInput = [
-    '.#.',
-    '..#',
-    '###',
-  ]
   expect(day17.part1(testInput)).toBe(112)
 })
 
 test('part2', () => {
-  const testInput = [
-    'b',
-  ]
-  expect(day17.part2(testInput)).toBe(testInput)
+  expect(day17.part2(testInput)).toBe(848)
 })
