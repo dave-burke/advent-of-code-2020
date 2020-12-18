71994204a20a10ac200d9c269eea96cfda0cd8c5
Dave Burke <dburke84@gmail.com>
2020-12-18 05:16:18 -0600

    Add part1 test cases

--- day18.test.js
+++ day18.test.js
@@ -1,11 +1,30 @@
 const { expect, test } = require('@jest/globals')
 const day18 = require('./day18')
 
-test('part1', () => {
-  const testInput = [
-    'a',
-  ]
-  expect(day18.part1(testInput)).toBe(testInput)
+describe('part1', () => {
+  test('1 + 2 * 3 + 4 * 5 + 6', () => {
+    expect(day18.part1('1 + 2 * 3 + 4 * 5 + 6')).toBe(71)
+  })
+
+  test('1 + (2 * 3) + (4 * (5 + 6))', () => {
+    expect(day18.part1('1 + (2 * 3) + (4 * (5 + 6))')).toBe(51)
+  })
+
+  test('2 * 3 + (4 * 5)', () => {
+    expect(day18.part1('2 * 3 + (4 * 5)')).toBe(26)
+  })
+
+  test('5 + (8 * 3 + 9 + 3 * 4 * 3)', () => {
+    expect(day18.part1('5 + (8 * 3 + 9 + 3 * 4 * 3)')).toBe(437)
+  })
+
+  test('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', () => {
+    expect(day18.part1('5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))')).toBe(12240)
+  })
+
+  test('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', () => {
+    expect(day18.part1('((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2')).toBe(13632)
+  })
 })
 
 test('part2', () => {
