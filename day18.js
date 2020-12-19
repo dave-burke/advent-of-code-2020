cc24f68ae7ba3e06c90f95851ae437644f3105de
Dave Burke <dburke84@gmail.com>
2020-12-18 23:24:15 -0600

    Implement part2

--- day18.js
+++ day18.js
@@ -39,8 +39,63 @@ function part1 (input) {
     .reduce((a, b) => a + b, 0)
 }
 
+// This is called the "Shunting Yard Algorithm"
+// I got it from a hint on Reddit, then implemented it based
+// on reading here: https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+function toRpn (input) {
+  const output = []
+  const operators = []
+  while (input.length > 0) {
+    const next = input.shift()
+    if (isFinite(Number(next))) {
+      output.push(Number(next))
+    } else if (next === '(') {
+      operators.unshift(next)
+    } else if (next === ')') {
+      while (operators[0] !== '(') {
+        output.push(operators.shift())
+      }
+      if (operators[0] === '(') {
+        operators.shift()
+      }
+    } else if (next === '+') {
+      operators.unshift('+')
+    } else if (next === '*') {
+      while (operators[0] === '+') {
+        output.push(operators.shift())
+      }
+      operators.unshift(next)
+    }
+  }
+  return [...output, ...operators]
+}
+
+// I made this rpn solver on my own
+function solveRpn (input) {
+  const stack = []
+  while (input.length > 0) {
+    const next = input.shift()
+    if (isFinite(Number(next))) {
+      stack.push(next)
+    } else {
+      const a = stack.pop()
+      const b = stack.pop()
+      if (next === '+') {
+        stack.push(a + b)
+      } else {
+        stack.push(a * b)
+      }
+    }
+  }
+  return stack.pop()
+}
+
 function part2 (input) {
   return input
+    .map(line => line.split('').filter(char => char !== ' '))
+    .map(equation => toRpn(equation))
+    .map(rpn => solveRpn(rpn))
+    .reduce((a, b) => a + b, 0)
 }
 
 module.exports = { part1, part2 }
