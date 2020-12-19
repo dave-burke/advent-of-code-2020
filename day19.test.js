df6dc790ebe07a0528763c1f701f1d0444091e76
Dave Burke <dburke84@gmail.com>
2020-12-19 08:46:55 -0600

    Add part1 test case

--- day19.test.js
+++ day19.test.js
@@ -2,10 +2,19 @@ const { expect, test } = require('@jest/globals')
 const day19 = require('./day19')
 
 test('part1', () => {
-  const testInput = [
-    'a',
-  ]
-  expect(day19.part1(testInput)).toBe(testInput)
+  const testInput = `0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: "a"
+5: "b"
+
+ababbb
+bababa
+abbbab
+aaabbb
+aaaabbb`
+  expect(day19.part1(testInput)).toBe(2)
 })
 
 test('part2', () => {
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const day = 18
-const part = 2
+const day = 19
+const part = 1
 
 const utils = require('./utils')
 const solution = require(`./day${day}`)
 
-utils.readInput(`day${day}.txt`).then((input) => solution[`part${part}`](input)).then(result => console.log(`Done: ${result}`))
+utils.readInputRaw(`day${day}.txt`).then((input) => solution[`part${part}`](input)).then(result => console.log(`Done: ${result}`))
