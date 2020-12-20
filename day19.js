87a642a8634a84c9317194d4efbd4b51acf0bad7
Dave Burke <dburke84@gmail.com>
2020-12-19 22:16:21 -0600

    Solve inputs, fail to finish on actual input

--- day19.js
+++ day19.js
@@ -10,34 +10,48 @@ function parseInput (rawInput) {
   return [rules, inputs]
 }
 
-function testOption (allRules, option, input) {
-  let prev
-  let check = option.split(' ')
-  while (prev !== check) {
-    prev = check
-    check = check.map(rule => rule.match(/"[ab]"/) ? rule[1] : allRules.get(Number(rule)))
+function resolve (allRules, rule) {
+  if (rule.startsWith('"')) {
+    return rule[1]
   }
-  return check === input
+  if (rule.includes('|')) {
+    const [x, y] = rule.split(' | ') // assumes only 2
+    return [resolve(allRules, x), resolve(allRules, y)]
+  } else {
+    return rule.split(' ').map(id => allRules.get(Number(id))).map(subRule => resolve(allRules, subRule))
+  }
 }
 
-function testInput (allRules, checkRule, input) {
-  for (const option of checkRule.split(' | ')) {
-    const isValid = testOption(allRules, option, input)
-    if (isValid) return true
+function validate (input, validGraph) {
+  const nextChar = input[0]
+  const nextValidator = validGraph[0]
+  if (nextChar === nextValidator) {
+    if (nextChar === undefined) return true
+    return validate(input.slice(1), validGraph.slice(1))
+  } else if (Array.isArray(nextValidator)) {
+    return nextValidator.some(it => validate(input, [...it, ...validGraph.slice(1)]))
+  } else {
+    return false
   }
-  return false
 }
 
 function part1 (rawInput) {
+  console.log(`${new Date().toISOString()} Parsing rules`)
   const [rules, inputs] = parseInput(rawInput)
-  let nValid = 0
-  for (const input of inputs) {
-    const isValid = testInput(rules, rules.get(0), input)
+  console.log(`${new Date().toISOString()} Building validation graph`)
+  const validGraph = resolve(rules, rules.get(0))
+  console.log(`${new Date().toISOString()} Validating inputs`)
+
+  let result = 0
+  for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i]
+    console.log(`${i} ${input}`)
+    const isValid = validate(input.split(''), validGraph)
     if (isValid) {
-      nValid += 1
+      result += 1
     }
   }
-  return nValid
+  return result
 }
 
 function part2 (input) {
